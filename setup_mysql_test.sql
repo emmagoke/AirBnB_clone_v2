358ca260e6ff4d6a613dbb594436c6bb279d5f6b
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-03-23 07:05:21 -0700

    Added setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,8 @@
+-- A database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- A new user hbnb_test (in localhost) and the password is hbnb_test_pwd
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- hbnb_test should have all privileges on the database hbnb_test_db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- hbnb_test should have SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
