06220980c077e00231c4ddb73335c95f6f0d4df1
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-23 07:19:21 -0700

    Added question 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,13 @@ at /
 """
 import os
 from flask import Flask
-
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
-def hbnb():
+def home():
     return "Hello HBNB!"
 
-port  = int(os.environ.get('PORT', 5000))
+
+port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
