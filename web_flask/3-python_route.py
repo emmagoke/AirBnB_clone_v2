13cc55fbba5ab73840df9e5e79ffc7fff0d31a5f
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-23 08:41:29 -0700

    Added question 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This is python script that starts a Flask web application
+The app is  listening on 0.0.0.0, port 5000
+Routes:
+
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ The home page route. """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ This is the hbnb route. """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    if '_' in text:
+        text = text.replace('_', ' ')
+
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<path:text>', strict_slashes=False)
+def python_text(text=None):
+    """ This /python or /python/text route."""
+    if text is None:
+        return 'Python is cool'
+    elif '_' in text:
+        text = text.replace('_', ' ')
+
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
