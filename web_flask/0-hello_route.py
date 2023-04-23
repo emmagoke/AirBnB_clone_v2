71ac8ca8e75bb5aebba2eaa418ca91512e9ff3e5
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-23 07:14:02 -0700

    Added question 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This python script starts a Flask web application tht diplays “Hello HBNB!”
+at /
+"""
+import os
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+port  = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
