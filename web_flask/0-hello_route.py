87986b7538282cad263635d052b37b6525a8326d
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-04-23 07:45:42 -0700

    Added question 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -14,5 +14,6 @@ def home():
     return "Hello HBNB!"
 
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
