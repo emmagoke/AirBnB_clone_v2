6a12ae9df05184aa8f29e3f97022d3af322398fc
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-19 16:57:05 -0700

    Third task in deploying static file

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script distributes archives to your web servers
+"""
+import os
+from fabric.api import put, run, env
+
+env.hosts = ['35.175.135.243', '54.157.136.243']
+
+
+def do_deploy(archive_path):
+    """ this function distributes archive on my web servers """
+
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        data_path = "/data/web_static/releases"
+        archive_name = archive_path.split('/')[-1]
+        ex_name = archive_name.split(".")[0]
+
+        put(archive_path, '/tmp/')
+        run("mkdir -p {0}/{1}/".format(data_path, ex_name))
+        run("tar -xzf /tmp/{} -C {0}/{1}/".format(data_path, ex_name))
+        run("rm /tmp/{}".format(archive_name))
+        run("mv {0}/{1}/web_static/* {0}/{1}/".format(data_path, ex_name))
+        run("rm -rf {}/{}/web_static".format(data_path, ex_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/{}/ /data/web_static/current".format(data_path, ex_name))
+        return True
+    except Exception:
+        return False
