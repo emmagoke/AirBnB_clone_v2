75e2aa59c631f4441068e74cded932fde0d5c1c1
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-19 15:54:19 -0700

    Second task in deploying static file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is a Fabric script that generates a .tgz archive from the contents
+of the web_static folder
+"""
+import os
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """ """
+
+    try:
+        if not os.path.isdir('versions'):
+            local('mkdir -p versions')
+
+        today_date = datetime.now().strftime("%Y%m%d%H%M%S")
+        output_file = f"versions/web_static_{today_date}.tgz"
+
+        local("tar -cvzf {} web_static".format(output_file))
+        return output_file
+    except Exception:
+        return None
