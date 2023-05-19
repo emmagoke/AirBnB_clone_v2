88614987cc59169dda1e8f18941b487d9d554204
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-19 09:11:58 -0700

    First task in deploying static file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,28 +11,15 @@ mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 
-html_="
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-"
+html_="<h1>Welcome to My World</h1>"
 echo "$html_" >> /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chown h-R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
-hbnb_url="
-location /hbnb_static/ {
-	alias /data/web_static/current/;
-	autoindex off;
-}
-"
+hbnb_url="\tlocation /hbnb_static/ {\t\t\nalias /data/web_static/current/;\t\t\nautoindex off;\t}"
 sudo sed -i "29i\ $hbnb_url" /etc/nginx/sites-avaliable/default
 
 service nginx restart
