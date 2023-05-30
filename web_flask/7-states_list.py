a2a25fa71a087958c543c642d18ebe065d938da0
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-05-30 02:50:57 -0700

    Corrected some mistakes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,7 @@ route: /cities_by_states -> Render an html page with the
 state id and state names
 """
 from flask import Flask, render_template
-from os import getenv
+import os
 from models import storage
 from models.state import State
 
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_appp():
+def close_appp(error):
     """ After each request you removes the current SQLAlchemy Session. """
     storage.close()
 
@@ -27,5 +27,5 @@ def states_list():
     return render_template('7-states_list.html', states=state_list)
 
 if __name__ == '__main__':
-    port = int(getenv('PORT'))
+    port = int(os.environ.get('PORT', '5000'))
     app.run(host='0.0.0.0', port=port)
